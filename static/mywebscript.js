cf85b4630674c26cc12c6ff8e3c42b85fb53ea8b
Leonard Muchuki <lennymkuu@gmail.com>
2024-01-26 12:15:30 +0300

    applied DRY principle on the mywebscript

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,24 +1,28 @@
-let runAddition = () => {
+let args = () => {
     let num1 = parseFloat(document.getElementById("num1").value);
     let num2 = parseFloat(document.getElementById("num2").value);
+    return [num1, num2]
+};
 
-    let result = num1 + num2;
+let poster = (result) => {
     document.getElementById("system_response").innerHTML = "Result: " + result;
+};
+
+let runAddition = () => {
+    let [num1, num2] = args()
+    let result = num1 + num2;
+    poster(result)
 };
 
 let runSubtraction = () => {
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
-
+    let [num1, num2] = args()
     let result = num1 - num2;
-    document.getElementById("system_response").innerHTML = "Result: " + result;
+    poster(result)
 };
 
 let runMultiplication = () => {
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
-
+    let [num1, num2] = args()
     let result = num1 * num2;
-    document.getElementById("system_response").innerHTML = "Result: " + result;
+    poster(result)
 };
 
